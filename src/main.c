454a6b735a9eac0af80d55d7cecaf73d91ab38da
Jose Barretto <joselucasbarretto@gmail.com>
2020-08-13 19:25:17 -0300

    added comments + code is cleaner

--- src/main.c
+++ src/main.c
@@ -3,14 +3,19 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+// CONSTANTS
 #define NUM_WORKERS 4
 #define MAX_INPUT_SIZE 100
 
+// GLOBAL VARIABLES
 unsigned long long int input[MAX_INPUT_SIZE]; // input storage
-int prime_count, input_idx, input_size, necessary_threads;
-pthread_mutex_t lock;
+int prime_count; // counts the prime number total
+int input_idx; // current input index
+int input_size; // total input size
+int necessary_threads; // number of input size
+pthread_mutex_t lock; // mutex lock
 
-// function to detec is integer x is prime
+// function to detect is integer x is prime
 bool is_prime(int x){
 	int i;
 
@@ -33,38 +38,30 @@ bool is_prime(int x){
 	return true;
 }
 
+// worker function
 void* worker(void* arg){
-	int *N = (int*)(arg);
-  	int M = (*N);
-
-  	//printf("entrei na thread %d\n", M);
-
+	
+	// iterates until every number in the input has been checked	
   	while(true){
-  		//printf("thread %d: trying to lock\n", M);
-		pthread_mutex_lock(&lock);
-		//printf("thread %d: locked\n", M);
-		//printf("thread %d: input idx = %d\n", M, input_idx);
+		
+		pthread_mutex_lock(&lock); // locks access to global variables
 
+		// loop stop condition	
 		if(input_idx >= input_size){
-			//printf("thread %d: idx is %d -> break\n", M, input_idx);
-			pthread_mutex_unlock(&lock);
-			break;
+			pthread_mutex_unlock(&lock); // unlocks access to global variables
+			break; // breaks out of loop
 		}
 
+		// checks if number is prime
 	 	if(is_prime(input[input_idx])){
 			prime_count++;
-			//printf("thread %d: input %llu is prime\n", M, input[input_idx]);
 		}
 
-		//else printf("thread %d: input %llu is NOT prime\n", M, input[input_idx]);
-		input_idx++;
-		//printf("thread %d: increased idx to %d\n", M, input_idx);
+		input_idx++; // updates the current input index
 
-		pthread_mutex_unlock(&lock);
-		//printf("thread %d: unlocked\n", M);
+		pthread_mutex_unlock(&lock); // unlocks access to global variables
   	}
 
-  	//printf("saindo da thread %d\n", M);
 	return NULL;
 }
 
@@ -73,9 +70,10 @@ int main() {
 
 	int i; // index variable
 	char c; // auxiliary char variable 
-	pthread_t workers[NUM_WORKERS];
-	int thread_id[NUM_WORKERS];
+	pthread_t workers[NUM_WORKERS]; // thread vector containing all the workers
+	int thread_id[NUM_WORKERS];	// thread ids vector 
 
+	// initializes global variables
 	prime_count = 0;
 	input_idx = 0;
 
@@ -89,12 +87,10 @@ int main() {
 
 	// stores the total input size
 	input_size = i;
-	//printf("input size: %d\n", input_size);
 
+	// calculates the necessary number of workers
 	if(input_size > NUM_WORKERS) necessary_threads = NUM_WORKERS;
 	else necessary_threads = input_size;
-
-	//printf("necessary threads: %d\n", necessary_threads);
 
 	// generates thread IDs
 	for (int i = 0; i < necessary_threads; i++) {
@@ -106,11 +102,11 @@ int main() {
 		pthread_create(&(workers[i]), NULL, worker, (void*)(&thread_id[i]));
 	}
 
-	// waits for threads
+	// waits for threads to finish
 	for (int i = 0; i < necessary_threads; i++) {
 		pthread_join(workers[i], NULL);
 	}
 
-	printf("%d\n", prime_count);
+	printf("%d\n", prime_count); // prints prime count
 	return 0;
 }
